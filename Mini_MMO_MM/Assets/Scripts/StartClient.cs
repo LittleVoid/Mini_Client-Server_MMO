b7396871c3848e75c1e21abc306f46d888d47206
LittleVoid <08evangelion08@gmail.com>
2025-03-01 16:32:36 +0100

    [*] Updated Srver and Client with Startter script

---
+++ Mini_MMO_MM/Assets/Scripts/StartClient.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class StartClient : MonoBehaviour
+{
+    public void Start()
+    {
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.StartClient();
+    }
+}
---
+++ Mini_MMO_MM/Assets/Scripts/StartServer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class StartServer : MonoBehaviour
+{
+    public void Start()
+    {
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.StartServer();
+    }
+}
