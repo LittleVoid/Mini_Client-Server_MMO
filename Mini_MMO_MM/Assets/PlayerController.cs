4f67608d7607aeef809cc0563706c1e17f2c5f0d
LittleVoid <08evangelion08@gmail.com>
2025-03-03 20:17:07 +0100

    [+] Added Colors to Players and synced it

--- Mini_MMO_MM/Assets/PlayerController.cs
+++ Mini_MMO_MM/Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,20 +6,56 @@ public class PlayerController : NetworkBehaviour
 {
     float speed = 10f;
     CharacterController characterController;
+    Material material;
+    MeshRenderer renderer;
+    NetworkVariable<Color> color = new NetworkVariable<Color>(writePerm: NetworkVariableWritePermission.Owner);
+
+    List<Color> colorList = new List<Color>()
+    {
+        Color.blue,
+        Color.red,
+        Color.green,
+        Color.yellow,
+    };
+
+    public override void OnNetworkSpawn()
+    {
+        characterController = GetComponent<CharacterController>();
+        renderer = GetComponent<MeshRenderer>();
+        material = renderer.material;
+        int randoIndex = Random.Range(0, colorList.Count);
+
+
+        color.OnValueChanged += OnColorChanged;
+
+        if (IsOwner)
+            color.Value = colorList[randoIndex];
+    }
 
     private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        material.color = color.Value;
     }
 
     private void Update()
     {
-        if (IsOwner && !IsServer)
+        if (!IsOwner) return;
+
+        Vector3 horizontalMovement = Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector3 forwardMovement = Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector3 motion = horizontalMovement + forwardMovement;
+        characterController.Move(motion);
+        
+
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            Vector3 horizontalMovement = Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            Vector3 forwardMovement = Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-            Vector3 motion = horizontalMovement + forwardMovement;
-            characterController.Move(motion);
+            int randoIndex = Random.Range(0, colorList.Count);
+            color.Value = colorList[randoIndex];
         }
+    }
+
+    private void OnColorChanged(Color previous, Color newColor)
+    {
+        material.color = newColor;
     }
 }
