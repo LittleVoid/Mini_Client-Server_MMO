55dea7e789a0bb4ddb714f8e16b11ca9a5bd5646
LittleVoid <08evangelion08@gmail.com>
2025-03-03 22:37:49 +0100

    [*] CleanUp

--- Mini_MMO_MM/Assets/Scripts/NetworkProjectile.cs
+++ Mini_MMO_MM/Assets/Scripts/NetworkProjectile.cs
@@ -20,6 +20,12 @@ public class NetworkProjectile : NetworkBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        NetworkObject.Despawn();
+        Debug.Log("Hit");
+    }
+
     private IEnumerator DestroyAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
